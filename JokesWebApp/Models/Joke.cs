13b0222d61e3c29761f32dc76ae3874f96bf51c5
Matthew Young <myoung89@uncc.edu>
2021-05-26 16:29:42 -0400

    Add project files.

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String Question { get; set; }
+        public String Answer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
